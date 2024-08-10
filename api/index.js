1c0a266c8d0f1a5091b49debb0b9d9e94b637a68
ShashankMaps <shashank.maps123@gmail.com>
2024-08-10 16:48:03 +0530

    DB connection is done

--- api/index.js
+++ api/index.js
@@ -1,8 +1,23 @@
 import express from 'express';
 //if we want to use this kind of import statement then we need to set "type":"module"  in pkj json file , niether we have to use "require"
-const app = express();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+//DB connection
+mongoose.connect(process.env.MONGO).then( ()=>{
+    console.log('Connection is Successfull');
+}).catch((error) =>{
+    console.log(error);
+})
+
+
+
+//starting the server
+const app = express();
 app.listen(3000,()=> {
         console.log("Server is running on port 3000 !");
     }
 );
+
+
