6bfd9714162826265540c538044f28bded6a097d
ShashankMaps <shashank.maps123@gmail.com>
2024-08-15 17:21:58 +0530

    created Sign in API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandaler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async  (req, res, next)=>{
     // console.log(req.body);
@@ -25,3 +27,43 @@ export const signup = async  (req, res, next)=>{
             next(error); //it will call our try catch middleware,
         }
 };
+
+
+//##############################
+//sign in controller
+
+export const signin =async (req, res, next) => {
+    const {email, password} = req.body;
+
+    try{
+
+        //check the valid email is exist iin db
+        const validUser = await User.findOne({ email });
+        
+        if(!validUser){
+            return next(errorHandaler(404,'User Not found '));
+        }
+        //hash the provided password
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        //if password is not correct
+        if(!validPassword){
+            return next(errorHandaler(401,'Wrong Credentials !'));
+        }
+
+        //if everything is ok then we  create JWT
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+        //destructuring the password that we are sending
+        const {password: pass, ...rest} = validUser._doc;
+        //now we save this  JWT in cookies
+        res.cookie('access_token', token, {
+            httpOnly: true,
+            expires: new Date(Date.now() + 2 * 24 * 60 * 60 * 1000) // 2 days from now
+          })
+          .status(200)
+          .json(rest);       
+    }
+
+    catch(error){
+        next(error); // calling the middelware
+    }
+}
