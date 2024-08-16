db125928c92d8f07efc3ab64fba378d263d447cb
ShashankMaps <shashank.maps123@gmail.com>
2024-08-16 23:56:54 +0530

    Add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice';  //aapn nav dil ah userReducer or userSlice same bcz export defaullt
-export default configureStore({
-  reducer: {user: userReducer},
+import { persistReducer} from 'redux-persist';
+import storage  from 'redux-persist/lib/storage';
+import persistStore from 'redux-persist/es/persistStore';
 
+const  rootReducer = combineReducers({user: userReducer});
+
+//to maintain user logged in 
+const persistConfig = {  
+    key:'root',
+    storage,
+    version: 1,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+//to maintain user logged in 
+
+export const store =  configureStore({
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
         serializableCheck:false, //we will not get any error for not serializing the variable in vour browser
-    })  
+    }),  
 });
+
+
+export const persistor =  persistStore(store);
